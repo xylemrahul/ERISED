46d23ef97d7d3f2a916322d9107228c65fc8bc71
Rahul <rajspeed.20@gmail.com>
2015-09-29 15:53:07 +0530

    FB, G+, Menu, Products, Material with Ripple effect and scrollable toolbar done

--- app/src/main/java/com/erised/ErisedMenuActivity.java
+++ app/src/main/java/com/erised/ErisedMenuActivity.java
@@ -1,10 +1,13 @@
 package com.erised;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.erised.base.BaseActivity;
 
@@ -12,20 +15,52 @@ import com.erised.base.BaseActivity;
  * Created by Rahul on 26/9/15.
  */
 
-public class ErisedMenuActivity extends BaseActivity {
-
+public class ErisedMenuActivity extends BaseActivity implements Animation.AnimationListener, View.OnClickListener {
 
     private RecyclerView menu;
+
+    // Animation
+    Animation animFadein;
+
+    TextView tshirt, sarees, sunglass, shop, ethnic, cosmetic, bags;
 
     @Override
     protected void initUI() {
         setContentView(R.layout.activity_erised_menu);
 
-
+        tshirt = (TextView) findViewById(R.id.tx_shirts);
+        sarees = (TextView) findViewById(R.id.tx_sarees);
+        sunglass = (TextView) findViewById(R.id.tx_sunglass);
+        shop = (TextView) findViewById(R.id.tx_shop);
+        ethnic = (TextView) findViewById(R.id.tx_ethnic);
+        cosmetic = (TextView) findViewById(R.id.tx_cosmetic);
+        bags = (TextView) findViewById(R.id.tx_bags);
     }
 
     @Override
     protected void initData() {
+
+        // load the animation
+        animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.bounce);
+        // set animation listener
+        animFadein.setAnimationListener(this);
+
+        tshirt.startAnimation(animFadein);
+        sarees.startAnimation(animFadein);
+        sunglass.startAnimation(animFadein);
+        shop.startAnimation(animFadein);
+        ethnic.startAnimation(animFadein);
+        cosmetic.startAnimation(animFadein);
+        bags.startAnimation(animFadein);
+
+        tshirt.setOnClickListener(this);
+        sarees.setOnClickListener(this);
+        sunglass.setOnClickListener(this);
+        shop.setOnClickListener(this);
+        ethnic.setOnClickListener(this);
+        cosmetic.setOnClickListener(this);
+        bags.setOnClickListener(this);
 
     }
 
@@ -49,5 +84,62 @@ public class ErisedMenuActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(ErisedMenuActivity.this,ProductsActivity.class);;
+
+        switch (view.getId()) {
+            case R.id.tx_shirts:
+
+                startActivity(intent);
+                break;
+
+            case R.id.tx_sarees:
+
+                startActivity(intent);
+                break;
+
+            case R.id.tx_sunglass:
+
+                startActivity(intent);
+                break;
+            case R.id.tx_shop:
+
+                startActivity(intent);
+                break;
+            case R.id.tx_ethnic:
+
+                startActivity(intent);
+                break;
+            case R.id.tx_cosmetic:
+
+                startActivity(intent);
+                break;
+
+            case R.id.tx_bags:
+
+                startActivity(intent);
+                break;
+        }
+
     }
 }
